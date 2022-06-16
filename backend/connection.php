a85d59a31be055dc642df3403585539f38f5a510
Bruce Felix <brucefelixm@gmail.com>
2022-06-16 09:15:45 +0300

    working on login page

---
+++ backend/connection.php
@@ -0,0 +1,14 @@
+<?php
+$user = 'cybernomand';
+$host = "localhost";
+$password = "D4yl!4D?Oef4ISYC";
+$database = "psychologist_booking_system";
+
+session_start();
+if($connection = mysqli_connect($host,$user,$password,$database)){
+    echo "<h3 style='color:green'>Connected successfully</h3>";
+}
+else{
+    echo"<h3 style='color:red'>Could not connect successfully</h3>" .mysqli_error($connection);
+}   
+?>
